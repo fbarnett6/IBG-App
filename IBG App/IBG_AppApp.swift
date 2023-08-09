333a378149d13307454e241102700abf04be216d
fbarnett6 <fbarnett6@yahoo.com>
2023-08-09 14:57:14 -0600

    Initial Commit

---
+++ IBG App/IBG_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBG_AppApp.swift
+//  IBG App
+//
+//  Created by fred barnett on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct IBG_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
